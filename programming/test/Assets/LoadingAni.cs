12a1b9e1456a2bce12176e69e09bc20d8b677804
SinJinWoong <31675791+SinJinWoong@users.noreply.github.com>
2018-03-31 16:43:51 +0900

    ㅇ
    ㅇ

---
+++ programming/test/Assets/LoadingAni.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingAni : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("LoadingUIEnd", 2);
+    }
+    void LoadingUIEnd()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ programming/test/Assets/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+    static public UIManager instance;
+
+    public GameObject SelectButton;
+    public Image image;
+
+    // Use this for initialization
+    void Start () {
+        image = transform.GetChild(0).GetComponent<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SeletMe()
+    {
+    }
+}
